cb65b0fddecba2261699cd606f36db7d78fe1154
thepriscillawang <61097085+thepriscillawang@users.noreply.github.com>
2022-10-29 11:19:54 -1000

    added rest of ingredients

--- src/SMOOthies/Scenes/Ingredients.gd
+++ src/SMOOthies/Scenes/Ingredients.gd
@@ -5,11 +5,22 @@ func _update_ingredient_label(ingredient):
 		$IngredientsPurchase/CurrMangoLabel.text = str(Global.curr_mango)
 	elif(ingredient ==  "pineapple"):
 		$IngredientsPurchase/CurrPineappleLabel.text = str(Global.curr_pineapple)
-#	elif(ingredient == "dragonfruit"):
+	elif(ingredient == "dragonfruit"):
+		$IngredientsPurchase/CurrDragonfruitLabel.text = str(Global.curr_dragonfruit)
+	elif(ingredient == "milk"):
+		$IngredientsPurchase/CurrMilkLabel.text = str(Global.curr_milk)
+	elif(ingredient == "ice"):
+		$IngredientsPurchase/CurrIceLabel.text = str(Global.curr_ice)
+	elif(ingredient == "cup"):
+		$IngredientsPurchase/CurrCupLabel.text = str(Global.curr_cup)
 
 func _ready():
 	_update_ingredient_label("mango")
 	_update_ingredient_label("pineapple")
+	_update_ingredient_label("dragonfruit")
+	_update_ingredient_label("milk")
+	_update_ingredient_label("ice")
+	_update_ingredient_label("cup")
 	print("Hello, ingredients!")
 
 func _load_Ingredients():
@@ -51,3 +62,50 @@ func _on_AddPineapple_pressed():
 	Global.curr_pineapple = Global.curr_pineapple + 1
 	_update_ingredient_label("pineapple")
 	print("+ Pineapple pressed. Total Pineapple: ",  Global.curr_pineapple)
+
+func _on_SubDragonfruit_pressed():
+	if (Global.curr_dragonfruit > 0 ):
+		Global.curr_dragonfruit = Global.curr_dragonfruit -1
+	_update_ingredient_label("dragonfruit")
+	print("- Dragonfruit pressed. Total Dragonfruit: ",  Global.curr_dragonfruit)
+
+func _on_AddDragonfruit_pressed():
+	Global.curr_dragonfruit = Global.curr_dragonfruit + 1
+	_update_ingredient_label("dragonfruit")
+	print("+ Dragonfruit pressed. Total Dragonfruit: ",  Global.curr_dragonfruit)
+
+func _on_SubMilk_pressed():
+	if (Global.curr_milk > 0 ):
+		Global.curr_milk = Global.curr_milk - 1
+	_update_ingredient_label("milk")
+	print("+ Milk pressed. Total Milk: ",  Global.curr_milk)
+
+func _on_AddMilk_pressed():
+	Global.curr_milk = Global.curr_milk + 1
+	_update_ingredient_label("milk")
+	print("+ Milk pressed. Total Milk: ",  Global.curr_milk)
+
+func _on_SubIce_pressed():
+	if (Global.curr_ice > 0):
+		Global.curr_ice = Global.curr_ice - 1
+	_update_ingredient_label("ice")
+	print("- Ice pressed. Total Ice: ",  Global.curr_ice)
+	
+func _on_AddIce_pressed():
+	Global.curr_ice = Global.curr_ice + 1
+	_update_ingredient_label("ice")
+	print("+ Ice pressed. Total Ice: ",  Global.curr_ice)
+
+func _on_SubCup_pressed():
+	if (Global.curr_cup > 0):
+		Global.curr_cup = Global.curr_cup - 1
+	_update_ingredient_label("cup")
+	print("- Cup pressed. Total Cup: ",  Global.curr_cup)
+
+
+
+func _on_AddCup_pressed():
+	Global.curr_cup = Global.curr_cup + 1
+	_update_ingredient_label("cup")
+	print("+ Cup pressed. Total Cup: ",  Global.curr_cup)
+
