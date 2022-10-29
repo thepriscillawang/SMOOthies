2a8ff3878d37512abf61a0d169f77219627bc586
thepriscillawang <61097085+thepriscillawang@users.noreply.github.com>
2022-10-29 10:35:16 -1000

    added pineapple and mango ingredient buttons

--- src/SMOOthies/Scenes/Global.gd
+++ src/SMOOthies/Scenes/Global.gd
@@ -5,6 +5,4 @@ var curr_day = 1
 var curr_money = 100.00
 
 var curr_mango = 0
-var curr_strawberry = 0
 var curr_pineapple = 0
-var curr_spinach = 0
--- src/SMOOthies/Scenes/Ingredients.gd
+++ src/SMOOthies/Scenes/Ingredients.gd
@@ -1,6 +1,15 @@
 extends Node2D
 
+func _update_ingredient_label(ingredient):
+	if (ingredient == "mango"):
+		$IngredientsPurchase/CurrMangoLabel.text = str(Global.curr_mango)
+	elif(ingredient ==  "pineapple"):
+		$IngredientsPurchase/CurrPineappleLabel.text = str(Global.curr_pineapple)
+#	elif(ingredient == "dragonfruit"):
+
 func _ready():
+	_update_ingredient_label("mango")
+	_update_ingredient_label("pineapple")
 	print("Hello, ingredients!")
 
 func _load_Ingredients():
@@ -12,10 +21,33 @@ func _on_ConfirmPurchase_pressed():
 	_play_sound()
 
 func _play_sound():
-	$IngredientsMusic.stream = load("res://Sounds/softbutton.wav")
+	$IngredientsMusic.stream = load("res://sounds/startsound.wav")
 	$IngredientsMusic.connect("finished", self, "_load_Main_scene")
 	$IngredientsMusic.play()
 
 func _load_Main_scene():
 #	get_tree().change_scene("res://Main.tscn") 
 	SceneLoader._change_scene("Main")
+
+#MANGO AREA
+func _on_SubMangos_pressed():
+	if (Global.curr_mango > 0 ):
+		Global.curr_mango = Global.curr_mango -1
+	_update_ingredient_label("mango")
+	print("- Mango pressed. Total Mango: ",  Global.curr_mango)
+
+func _on_AddMangos_pressed():
+	Global.curr_mango = Global.curr_mango + 1
+	_update_ingredient_label("mango")
+	print("+ Mango pressed. Total Mango: ",  Global.curr_mango)
+	
+func _on_SubPineapple_pressed():
+	if (Global.curr_pineapple > 0 ):
+		Global.curr_pineapple = Global.curr_pineapple -1
+	_update_ingredient_label("pineapple")
+	print("- Pineapple pressed. Total Pineapple: ",  Global.curr_pineapple)
+
+func _on_AddPineapple_pressed():
+	Global.curr_pineapple = Global.curr_pineapple + 1
+	_update_ingredient_label("pineapple")
+	print("+ Pineapple pressed. Total Pineapple: ",  Global.curr_pineapple)
