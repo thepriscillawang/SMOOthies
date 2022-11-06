010840c1dfb43a3696d1b74bfcc3399061636be0
thepriscillawang <61097085+thepriscillawang@users.noreply.github.com>
2022-11-05 21:05:22 -1000

    added clock

--- src/SMOOthies/Scenes/Simulation.gd
+++ src/SMOOthies/Scenes/Simulation.gd
@@ -6,8 +6,34 @@ func _ready():
 	pass # Replace with function body.
 
 func _on_FastForwardButton_pressed():
+	go_to_report()
+	
+func go_to_report():
 	if (Global.curr_day < Global.total_days):
 		# generate whether or not there is random ingredient event
 		SceneLoader._change_scene("EndOfDayReport")
 	else:
 		SceneLoader._change_scene("Report")
+
+func _process(delta):
+	var text = str($Timer.time_left)
+#
+var am_pm = "am"
+var hour = 8
+var minute = 0
+var week = 0
+
+func _physics_process(_delta):
+	$Clock.text = str(hour) + " : " + str(int(minute)) + " " + am_pm
+	minute += 1
+	if minute == 60:
+		minute = 0
+		hour += 1
+	if hour == 12:
+		am_pm = "pm"
+	if hour == 13:
+		hour = 1
+
+	
+func _on_Timer_timeout():
+	go_to_report()
